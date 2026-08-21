9b038a69da67e7ba8b987386cba9242da3b22de4
SangfoidEns <90978826+SangfoidEns@users.noreply.github.com>
2026-08-21 11:40:48 +0200

    Replace Tailwind config with application config

--- js/config.js
+++ js/config.js
@@ -1,19 +1,8 @@
-tailwind.config = {
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        iosBg: '#090A0F',
-        iosCard: 'rgba(22, 27, 38, 0.65)',
-        iosCardBorder: 'rgba(255, 255, 255, 0.12)',
-        iosAccent: '#007AFF',
-        iosPeach: '#FF9F0A',
-        iosEmerald: '#30D158',
-        iosViolet: '#BF5AF2',
-        iosRose: '#FF453A',
-        iosAmber: '#FFD60A',
-        iosCyan: '#64D2FF'
-      }
-    }
-  }
+export const CONFIG = {
+  STORAGE_KEYS: {
+    RAW_DATA: 'hms_raw_data',
+    MANUAL_EXPENSES: 'hms_manual_expenses'
+  },
+  DEFAULT_CURRENCY: '€',
+  DAYS_MAP: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
 };
