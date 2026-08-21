8d3284fac069f32bafa869e8e506c30a625d0f0f
SangfoidEns <90978826+SangfoidEns@users.noreply.github.com>
2026-08-21 12:42:31 +0200

    Add MANUAL_TRANSACTIONS key to STORAGE_KEYS

--- js/config.js
+++ js/config.js
@@ -2,8 +2,9 @@ export const CONFIG = {
   STORAGE_KEYS: {
     RAW_DATA: 'hms_raw_data',
     PRICING: 'hms_pricing_settings',
-    MY_TRANSACTIONS: 'hms_my_transactions'
+    MANUAL_TRANSACTIONS: 'hms_manual_transactions'
   },
+  DEFAULT_CURRENCY: '€',
   DAYS_MAP: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
   DEFAULT_PRICING: {
     'BANNAN': 600,
